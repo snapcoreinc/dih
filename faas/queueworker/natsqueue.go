845f01a2457e8049257a60469097b05eae36ea5b
Olivier Refalo <orefalo@snapcore.com>
2020-06-18 09:10:38 +0400

    fix

--- faas/queueworker/natsqueue.go
+++ faas/queueworker/natsqueue.go
@@ -1,9 +1,12 @@
 package queueworker
 
 import (
+	"fmt"
 	stan "github.com/nats-io/stan.go"
+	"log"
 	"sync"
 	"time"
+	"github.com/openfaas/nats-queue-worker/handler"
 )
 
 // NATSQueue queue for work
@@ -24,4 +27,32 @@ type NATSQueue struct {
 
 	// Topic to respond to
 	Topic string
+}
+
+// CreateNATSQueue ready for asynchronous processing
+func CreateNATSQueue(address string, port int, clusterName, channel string, clientConfig handler.NATSConfig) (*NATSQueue, error) {
+	var err error
+	natsURL := fmt.Sprintf("nats://%s:%d", address, port)
+	log.Printf("Opening connection to %s\n", natsURL)
+
+	clientID := clientConfig.GetClientID()
+
+	// If 'channel' is empty, use the previous default.
+	if channel == "" {
+		channel = "faas-request"
+	}
+
+	queue1 := NATSQueue{
+		ClientID:       clientID,
+		ClusterID:      clusterName,
+		NATSURL:        natsURL,
+		Topic:          channel,
+		maxReconnect:   clientConfig.GetMaxReconnect(),
+		reconnectDelay: clientConfig.GetReconnectDelay(),
+		ncMutex:        &sync.RWMutex{},
+	}
+
+	err = queue1.connect()
+
+	return &queue1, err
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/snapcoreinc/dih
 
 go 1.13
 
-require github.com/nats-io/stan.go v0.6.0
+require (
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/nats-io/nats-server/v2 v2.1.7 // indirect
+	github.com/nats-io/nats-streaming-server v0.17.0 // indirect
+	github.com/nats-io/stan.go v0.6.0
+	github.com/openfaas/faas v0.0.0-20200615184600-d0ebb19fd972 // indirect
+	github.com/openfaas/nats-queue-worker v0.0.0-20200512211843-8e9eefd5a320
+)
