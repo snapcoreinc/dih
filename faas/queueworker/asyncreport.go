9c71d3e1fbcad71ab8b2eef6febedf21a79225c6
Olivier Refalo <orefalo@snapcore.com>
2020-07-10 13:04:09 +0400

    removed queueworker package

--- faas/queueworker/asyncreport.go
+++
@@ -1,9 +0,0 @@
-package queueworker
-
-type AsyncReport struct {
-	FunctionNameSpace string  `json:"namespace"`
-	FunctionName      string  `json:"name"`
-	StatusCode        int     `json:"statusCode"`
-	TimeTaken         float64 `json:"timeTaken"`
-	// TODO: Return URL to fetch status
-}
